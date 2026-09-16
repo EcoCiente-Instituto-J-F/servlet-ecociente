61b9f2a992761503463e4db75f4dbfc6681a04f2
gbn2277 <gsilva.oliveira0207@gmail.com>
2026-09-15 23:09:08 -0300

    feat: adiciona metodo de insert em UsuarioDAO.java

--- src/main/java/com/example/ecociente/dao/UsuarioDAO.java
+++ src/main/java/com/example/ecociente/dao/UsuarioDAO.java
@@ -1,4 +1,56 @@
 package com.example.ecociente.dao;
 
+import com.example.ecociente.conexao.ConexaoBD;
+import com.example.ecociente.model.Usuario;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 public class UsuarioDAO {
+
+    //=======================MÉTODOS CREATE=======================\\
+
+    public boolean inserir(Usuario usuario){
+        String sql = """
+            INSERT INTO usuario
+                (id_usuario, nome, email, senha_hash, data_cadastro, id_endereco, id_tipo_usuario)
+            VALUES 
+                (?, ?, ?, ?, ?, ?, ?)
+            """;
+
+        try (
+            Connection conexao = ConexaoBD.conectar();
+            PreparedStatement comando =
+                    conexao.prepareStatement(sql);
+        ){
+
+            comando.setInt(1, usuario.getIdUsuario());
+            comando.setString(2, usuario.getNome());
+            comando.setString(3, usuario.getEmail());
+            comando.setString(4, usuario.getSenhaHash());
+            comando.setDate(
+                    5,
+                    Date.valueOf(LocalDate.now())
+                    );
+            comando.setInt(6, usuario.getIdEndereco());
+            comando.setInt(7, usuario.getIdTipoUsuario());
+
+
+
+            int linhasAfetadas = comando.executeUpdate();
+
+            return linhasAfetadas > 0;
+        } catch (SQLException e) {
+            System.out.println(
+                    "Erro ao inserir tarefa" +
+                            e.getMessage()
+            );
+
+            return false;
+        }
+    }
+
 }
