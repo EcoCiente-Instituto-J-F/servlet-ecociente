c9408635cc75e60d3ebba19f0c51e729ad6e8628
gbn2277 <gsilva.oliveira0207@gmail.com>
2026-09-23 00:32:15 -0300

    feat: adiciona arquivo EnderecoDAO.java e o metodo de inserir um novo endereco

---
+++ src/main/java/com/example/ecociente/dao/EnderecoDAO.java
@@ -0,0 +1,50 @@
+package com.example.ecociente.dao;
+
+import com.example.ecociente.conexao.ConexaoBD;
+import com.example.ecociente.model.Endereco;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class EnderecoDAO {
+    //=======================MÉTODOS CREATE=======================\\
+
+    // Insere novo endereco com complemento
+    public boolean inserir(Endereco endereco){
+        String sql = """
+                INSERT INTO endereco 
+                    (id_endereco, cep, cidade, estado, bairro, rua, numero, complemento)
+                VALUES
+                    (?, ?, ?, ?, ?, ?, ?, ?)
+                """;
+
+        try (
+                Connection conexao = ConexaoBD.conectar();
+                PreparedStatement comando =
+                        conexao.prepareStatement(sql)
+        ){
+            comando.setInt(1, endereco.getIdEndereco());
+            comando.setString(2, endereco.getCep());
+            comando.setString(3, endereco.getCidade());
+            comando.setString(4, endereco.getEstado());
+            comando.setString(5, endereco.getBairro());
+            comando.setString(6, endereco.getBairro());
+            comando.setInt(7, endereco.getNumero());
+            comando.setString(8, endereco.getComplemento());
+
+            int linhasAfetadas = comando.executeUpdate();
+
+            return linhasAfetadas > 0;
+
+
+
+
+        }catch (SQLException sqle){
+            System.out.println("Erro ao inserir endereço" + sqle.getMessage());
+
+            return false;
+        }
+    }
+}
